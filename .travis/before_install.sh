2c38ffd5da2182174f998564c47597ff1c2125d3
Henrik Skupin <mail@hskupin.info>
2015-06-05 18:35:51 +0200

    screen size debugging

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,8 +2,9 @@
 set -ev
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    /usr/bin/xrandr
     /sbin/start-stop-daemon --start --quiet --make-pidfile --pidfile /tmp/custom_xvfb_99.pid \
-         --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x768x16
+         --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x960x16
 
     sudo apt-get update -qq
     sudo apt-get install python-pip python-virtualenv
