6c84e2cf7f954bef5df88b8f54fdecf0e3ce0514
Henrik Skupin <mail@hskupin.info>
2015-06-05 22:32:55 +0200

    remove call to xrandr

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,7 +2,6 @@
 set -ev
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    /usr/bin/xrandr
     /sbin/start-stop-daemon --start --quiet --make-pidfile --pidfile /tmp/custom_xvfb_99.pid \
          --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 
